8597cf205cee39d82f48cd2df8b3aec0068a269f
Vitalii Lytvynov <urutseg@gmail.com>
2018-07-28 19:29:12 +0300

    added file for chart

--- src/lc.py
+++ src/lc.py
@@ -9,12 +9,13 @@ from gensim.parsing import remove_stopwords
 from gensim.utils import tokenize
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS, CountVectorizer
 
-from src.custom_stop_words import csw
+from src.custom_stop_words import CSW
 
 
 class LyricsCharter(object):
     """Class that hadles lyrics analysis for previously downloaded file"""
     def __init__(self, lyrics_file, artist_name):
+        """Reads previously downloaded json and processes its content to a dataframe"""
         df = pd.read_json(lyrics_file)
         df.rename(columns={"name": "album"}, inplace=True)
         df["year"] = pd.to_numeric(df["year"], downcast="integer")
@@ -44,11 +45,13 @@ class LyricsCharter(object):
             lambda x: len(set(list(tokenize(remove_stopwords(x), lower=True)))))
 
     def songs_by_decade(self):
+        """Produces a chart for song count"""
         songs_by_decade = self.data.groupby("decade")[["song"]].count()
         songs_by_decade.reset_index(inplace=True)
         sns.barplot(data=songs_by_decade, x="decade", y="song")
 
     def word_counts_density(self):
+        """Produces two histograms for songs' word counts"""
         fig = plt.figure(figsize=(15, 5))
         ax = fig.subplots(1, 2)
         sns.distplot(self.data.word_count, ax=ax[0], kde=False)
@@ -56,6 +59,7 @@ class LyricsCharter(object):
         # return ax
 
     def word_counts_trend(self):
+        """Charts for lexical density over years"""
         gb = self.data.groupby("year")["word_count", "unique_words"].mean()
         gb["lex_density"] = gb["unique_words"] / gb["word_count"]
         g = gb.unstack().reset_index()
@@ -67,46 +71,18 @@ class LyricsCharter(object):
         # return ax
 
     def word_counts_vs_unique(self):
+        """Produces three variations of a jointplot over word counts"""
         sns.jointplot(data=self.data, x="unique_words", y="word_count")
         sns.jointplot(data=self.data, x="unique_words",
                     y="word_count", kind="hex")
         sns.jointplot(data=self.data, x="unique_words",
                     y="word_count", kind="kde")
 
-    def words_by_year(self):
-        words_by_year = self.data[["decade", "lyrics"]]
-        words_by_year.columns = ["year", "lyrics"]
-
-        wc_by_year = []
-        for year, l in words_by_year.iterrows():
-            l_ser = pd.Series(
-                list(tokenize(remove_stopwords(l["lyrics"]), lower=True)))
-            idf = pd.DataFrame(l_ser.groupby(l_ser).count(), columns=["count"])
-            idf["decade"] = l["year"]
-            wc_by_year.append(idf)
-
-        wc_by_year_df = pd.DataFrame(columns=["count"],
-                                    index=pd.MultiIndex(names=[None, "decade"], levels=[["and"], [1984]], labels=[[0], [0]]))
-
-        for el in wc_by_year:
-            wc_by_year_df = wc_by_year_df.add(
-                el.set_index("decade", append=True), fill_value=0)
-
-        wc_by_year_df.index = wc_by_year_df.index.swaplevel(0, 1)
-        wc_by_year_df.dropna(inplace=True)
-        wc_by_year_df["count"] = wc_by_year_df["count"].astype("int")
-        wc_by_year_df = wc_by_year_df.reset_index()
-        wc_by_year_df.columns = ["decade", "word", "count"]
-
-        top10_by_year = wc_by_year_df.groupby(["decade"])
-        plot_dfs = [{"decade": y, "df": g.set_index("word").drop("decade", axis=1).nlargest(
-            10, "count").sort_values("count")} for y, g in top10_by_year]
-        return plot_dfs
-
-    def top_n_words_heatmap(self, n=10, ngrams=(1, 1)):
+    def top_n_words_heatmap(self, n=10, ngrams=(1, 1), by_decade=True):
+        """Produces a heatmap for top N words or their combinations over the years"""
         df = self.data.copy()
         df.reset_index(drop=True, inplace=True)
-        sw = ENGLISH_STOP_WORDS.union(csw)
+        sw = ENGLISH_STOP_WORDS.union(CSW)
         c = CountVectorizer(stop_words=sw, ngram_range=ngrams)
 
         m = c.fit_transform(df.lyrics)
@@ -122,7 +98,11 @@ class LyricsCharter(object):
         total_words = pd.concat(words_df, axis=1, sort=False)
 
         trans = total_words.T
-        trans["_year"] = df["decade"].fillna(0)
+        if by_decade:
+            trans["_year"] = df["decade"].fillna(0)
+        else:
+            trans["_year"] = df["year"].fillna(0)
+
         trans["_year"] = pd.to_numeric(trans["_year"].fillna(0), downcast="integer")
 
         words_by_year = trans.groupby("_year").sum()
@@ -135,7 +115,7 @@ class LyricsCharter(object):
         top10_df.sort_values(by="sum", ascending=False, inplace=True)
         top10_df.drop("sum", axis=1, inplace=True)
 
-        fig = plt.figure(figsize=(15, 10))
+        fig = plt.figure(figsize=(10, 7))
         sns.heatmap(top10_df.T, square=True, cmap="YlGnBu", cbar_kws={"orientation":"horizontal"})
 
 
